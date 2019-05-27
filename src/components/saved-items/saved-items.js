b7effa029cc36553bfefeaaa890c2a4665a074f6
Carolina Knoll <carolinasknoll@gmail.com>
2019-05-27 09:58:09 -0300

    Toggle saved-items-panel open/closed with button onClick

--- src/components/saved-items/saved-items.js
+++ src/components/saved-items/saved-items.js
@@ -9,16 +9,24 @@ export default class SavedItems extends Component {
   }
 
   handleSavedItemsButtonClick = () => {
-    // this.props.handleSavedItemsButtonClick();
+    let savedItemsPanel = document.querySelector('.saved-items-panel');
+
+    savedItemsPanel.classList.contains('open')
+      ? savedItemsPanel.classList.remove('open')
+      : savedItemsPanel.classList.add('open');
   }
 
 	render() {
     return (
       <div className="saved-items-container">
         <button className="button-default" onClick={this.handleSavedItemsButtonClick}>
-        <i className="far fa-save"></i>
+          <i className="far fa-save"></i>
           View saved items
         </button>
+
+        <div className="saved-items-panel">
+          <h2>Saved items</h2>
+        </div>
       </div>
     );
 	}
