4ec01deb1fc0645fba2d51bd80347c1975497c91
José Neto <sputinykster@gmail.com>
2019-07-08 15:41:11 +0100

    Removing React Router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter, Route} from 'react-router-dom';
 
 import './index.css';
 
 import AwesomeGamified from './containers/awesome-gamified';
 
 ReactDOM.render(
-  <HashRouter>
-    <Route exact path='/' component={AwesomeGamified} />
-  </HashRouter>,
+  <AwesomeGamified />,
   document.getElementById('root'),
 );
