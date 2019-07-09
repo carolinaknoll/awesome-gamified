26bb07bbddd748f95b5eede93d9cfc4cf72e653c
Anderson Souza <andersom.eugenio@gmail.com>
2019-07-09 10:14:28 -0300

    refatorado webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -9,7 +9,7 @@ module.exports = merge(common, {
   devServer: {
     contentBase: path.join(__dirname, "public/"),
     port: 3000,
-    publicPath: "http://localhost:3000/dist/",
+    publicPath: "/",
     hotOnly: true
   },
   plugins: [new webpack.HotModuleReplacementPlugin()]
